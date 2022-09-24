059ea349e5d7d32032e99cd799b05ca93a4a979c
Hetvi Sharma <20IT142@charusat.edu.in>
2022-09-24 15:21:43 +0530

    MyAlert Commit

--- Desktop/FSWD/20IT142StudentManagementSystemFrontend-main/src/components/MyAlert.js
+++ Desktop/FSWD/20IT142StudentManagementSystemFrontend-main/src/components/MyAlert.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
+// import React from "react";
+// import { Alert } from "react-bootstrap";
 
-export default function MyAlert(props) {
-  return (
-    props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
-  );
-}
+// export default function MyAlert(props) {
+//   return (
+//     props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
+//   );
+// }
